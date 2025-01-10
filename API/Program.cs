05dcc8eeb559971959a657b8e455b79c79230e5a
DIWAKAR S <diwakar@excelgoodies.com>
2025-01-10 10:28:29 +0530

    UPDATE COMMIT

---
+++ API/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Disable HTTPS redirection
+//builder.Services.AddHttpsRedirection(options =>
+//{
+//    options.HttpsPort = null; // Disable the middleware
+//});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
